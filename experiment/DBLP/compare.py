b9cfc6c199ad7f9f7f5cd3c60fa50655a454462a
Zachary Adler <alsozatch@gmail.com>
2022-06-07 23:35:42 -0700

    more tests in compare

--- experiment/DBLP/compare.py
+++ experiment/DBLP/compare.py
@@ -1,4 +1,5 @@
 from pagerank_motif_direct import *
+import time
 
 # code adapted from motif_construct_direct to find the adjacency matrix without computing a motif
 def get_adjacency_matrix(network_file):
@@ -54,24 +55,45 @@ def compute_pagerank(adjacency_matrix, alpha):
     v = pageRank(p, M, pr)
     return v
 
+# pagerank_vals contains the original pagerank values for original citation_network.txt
+# networks_list is a list of indices from 0 to 29 choosing which modified citation networks to use
+# motif is 'M1' or ... 'M7'
+# alpha in 0 to 1
+# out_file will have the errors written in
+def compare(pagerank_vals, networks_list, motif, alpha, out_file):
+    start = time.time()
+
+    f = open(out_file, 'w')
+    MPR_adj, MPR_id = construct_motif('data/DBLP/citation_network.txt', 1, motif, alpha)
+    MPR = compute_pagerank(MPR_adj, alpha)
+    for i in networks_list:
+        PR_new_adj, PR_new_id = get_adjacency_matrix("data/DBLP/citation_network_modified_%d.txt" % (i))
+        MPR_new_adj, MPR_new_id = construct_motif("data/DBLP/citation_network_modified_%d.txt" % (i), 1, motif, alpha)
+    
+        PR_new = compute_pagerank(PR_new_adj, alpha)
+        MPR_new = compute_pagerank(MPR_new_adj, alpha)
+    
+        error_PR = np.sum(np.absolute(PR - PR_new[:len(PR)]))
+        error_MPR = np.sum(np.absolute(MPR - MPR_new[:len(MPR)]))
+    
+        f.write("%f;%f\n" % (error_PR, error_MPR))
+        print("---compare has taken %f seconds" % (time.time() - start))
+
 if __name__ == '__main__':
+    start = time.time()
+    alpha = 0.15
     PR_adj, PR_id = get_adjacency_matrix('data/DBLP/citation_network.txt')
-    PR_0_adj, PR_0_id = get_adjacency_matrix('data/DBLP/citation_network_modified_0.txt')
-    MPR_adj, MPR_id = construct_motif('data/DBLP/citation_network.txt', 1, 'M6', 0.4)
-    MPR_0_adj, MPR_0_id = construct_motif('data/DBLP/citation_network_modified_0.txt', 1, 'M6', 0.4)
+    PR = compute_pagerank(PR_adj, alpha)
     
-    print(array_equal(PR_id, MPR_id))
-    print(array_equal(PR_0_id, MPR_0_id))
+    motif = 'M1'
+    compare(PR, range(5), motif, alpha, "output/DBLP/%s_errors.txt" % (motif))
+    print("main has taken %f seconds" %(time.time() - start))
     
-    PR = compute_pagerank(PR_adj, 0.4)
-    PR_0 = compute_pagerank(PR_0_adj, 0.4)
-    MPR = compute_pagerank(MPR_adj, 0.4)
-    MPR_0 = compute_pagerank(MPR_0_adj, 0.4)
+    motif = 'M4'
+    compare(PR, range(5, 10), motif, alpha, "output/DBLP/%s_errors.txt" % (motif))
+    print("main has taken %f seconds" %(time.time() - start))
     
-    
-    error_PR = np.sum(np.absolute(PR - PR_0[:len(PR)]))
-    error_MPR = np.sum(np.absolute(MPR - MPR_0[:len(MPR)]))
-    
-    print(error_PR)
-    print(error_MPR)
+    motif = 'M7'
+    compare(PR, range(10, 15), motif, alpha, "output/DBLP/%s_errors.txt" % (motif))
+    print("main has taken %f seconds" %(time.time() - start))
     
